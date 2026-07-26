ab7b593c9f4d8067dcde261b4828c774f01b4d95
cumar-dev <your_email@example.com>
2026-07-26 15:04:40 +0300

    first commit

---
+++ React-with-typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import UserCard from "./UserCard";
+function App() {
+  return (
+    <>
+      <UserCard
+        name="T-shirt"
+        description="Welcome to these new T-shirts. We have different brands like Nike, Adidas, and Puma."
+        price={20}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ React-with-typescript/src/UserCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+interface UserCardProps {
+    name: string;
+    description: string;
+    price: number;
+}
+const UserCard = (props: UserCardProps) => {
+    console.log(props);
+  return (
+   <div style={{ padding: 0, margin: 0 }}>
+    <h1>{props.name.toUpperCase()}</h1>
+    <p>{props.description}</p>
+    <strong>${props.price.toFixed(2)}</strong>
+    </div>
+  )
+}
+
+export default UserCard;
